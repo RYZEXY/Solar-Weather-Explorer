6c59ffa3c8d6e0fbe65b3632d31c38587d69bf4d
RYZEXY <sunfred88@gmail.com>
2023-06-30 12:46:20 -0400

    vite change

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: "/solar-weather-explorer/",
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    mimeTypes: {
+      "text/javascript": ["js", "mjs"],
+      "text/jsx": ["jsx"]
+    }
   }
-})
+});
