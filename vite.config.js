aae011573ad8cc9ee3661368c33910f7cf8bad34
RYZEXY <sunfred88@gmail.com>
2023-06-30 12:34:51 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/solar-weather-explorer/",
-
+  base: "/solar-weather/",
   plugins: [react()],
   server: {
     port: 3000
